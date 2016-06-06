58aaea0af9c711e60d9f29e94a61c4c6aedc3d78
slagogonzalez <slagogonzalez@atanasoff28.danielcastelao.org>
2016-06-06 09:37:56 +0200

    Clases Refactorizadas y añadido javadoc

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -1,9 +1,10 @@
 package examenfinalparte1;
 
 import java.io.IOException;
+
 /**
  * @author Sergio
-*/
+ */
 public class Prin {
 
     public static void main(String[] args) throws IOException {
@@ -12,18 +13,21 @@ public class Prin {
          */
         Principal obj = new Principal();
         /**
-         * Mostramos por pantalla Es: y concatenamos el metodo SumaDistintos introduciendole 2 valores
+         * Mostramos por pantalla Es: y concatenamos el metodo SumaDistintos
+         * introduciendole 2 valores
          */
         System.out.println("Es: " + obj.SumaNumDiferentes(10, 20));
         /**
-         * Mostramos por pantalla Es: y concatenamos el metodo sumaMismoNum introduciendole 1 valor
+         * Mostramos por pantalla Es: y concatenamos el metodo sumaMismoNum
+         * introduciendole 1 valor
          */
-        
+
         System.out.println("Es: " + obj.sumaIgual(10));
         /**
-         * Mostramos por pantalla Es: y concatenamos el metodo menosUno que retorna -1
+         * Mostramos por pantalla Es: y concatenamos el metodo menosUno que
+         * retorna -1
          */
-        
+
         System.out.println("Es: " + obj.menosUno());
     }
 }
--- src/examenfinalparte1/Principal.java
+++ src/examenfinalparte1/Principal.java
@@ -1,47 +1,55 @@
 package examenfinalparte1;
+
 /**
  * @author sergio
- 
+ *
  */
 public class Principal {
 
     int i;
- /**
+
+    /**
      * Metodo el cual realiza una suma de 2 numeros
-     * @param num1 valor del num 1 
-     * @param num2 valor del num 2 
+     *
+     * @param num1 valor del num 1
+     * @param num2 valor del num 2
      */
- public int suma(int num1, int num2) {
+    public int suma(int num1, int num2) {
         return num1 + num2;
     }
- /**
-  * Este metodo va a sumar dos numeros distintos utilizando el metodo Suma
-  * @param num1 valor del 1 numero
-  * @param num2 valor del 2 numero
-  * @return Por ultimo retorna el valor de la suma de ambos recogida en i
-  */
+
+    /**
+     * Este metodo va a sumar dos numeros distintos utilizando el metodo Suma
+     *
+     * @param num1 valor del 1 numero
+     * @param num2 valor del 2 numero
+     * @return Por ultimo retorna el valor de la suma de ambos recogida en i
+     */
     public int SumaNumDiferentes(int num1, int num2) {
         i = suma(num1, num2);
         return i;
     }
-/**
- * Este metodo va a sumar 2 numeros iguales, y solo se le introduce un parametro
- * Utilizamos otra vez el metodo Suma
- * @param num Valor del numero que se introduce 
- * @return Retorna el valor de la suma recogida en i
- */
+
+    /**
+     * Este metodo va a sumar 2 numeros iguales, y solo se le introduce un
+     * parametro Utilizamos otra vez el metodo Suma
+     *
+     * @param num Valor del numero que se introduce
+     * @return Retorna el valor de la suma recogida en i
+     */
     public int sumaIgual(int num) {
         i = suma(num, num);
         return i;
     }
+
     /**
- * Este metodo simplemente nos devuelve el valor -1
- * @return 
- */
+     * Este metodo simplemente nos devuelve el valor -1
+     *
+     * @return
+     */
     public int menosUno() {
         i = -1;
         return i;
     }
 
-  
 }
