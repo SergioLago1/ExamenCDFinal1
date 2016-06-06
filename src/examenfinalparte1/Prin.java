c45ac12b964a22645a8f844b27c73ae5935fd6ba
slagogonzalez <slagogonzalez@atanasoff28.danielcastelao.org>
2016-06-06 09:32:26 +0200

    Modifico la clase main para mostrar las salidas de pantalla del programa explicadas

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -1,19 +1,29 @@
 package examenfinalparte1;
 
 import java.io.IOException;
-/*@param creamos un constructor
+/**
+ * @author Sergio
 */
 public class Prin {
 
     public static void main(String[] args) throws IOException {
-        Principal obj = new Principal(10, 20);
-        int n = obj.i;
-        System.out.println("Es: " + n);
-        Principal nm = new Principal(10);
-        int m = nm.i;
-        System.out.println("Es: " + m);
-        Principal mn = new Principal();
-        int nn = mn.i;
-        System.out.println("Es: " + nn);
+        /**
+         * Aqui creamos un objeto de tipo principal
+         */
+        Principal obj = new Principal();
+        /**
+         * Mostramos por pantalla Es: y concatenamos el metodo SumaDistintos introduciendole 2 valores
+         */
+        System.out.println("Es: " + obj.SumaNumDiferentes(10, 20));
+        /**
+         * Mostramos por pantalla Es: y concatenamos el metodo sumaMismoNum introduciendole 1 valor
+         */
+        
+        System.out.println("Es: " + obj.sumaIgual(10));
+        /**
+         * Mostramos por pantalla Es: y concatenamos el metodo menosUno que retorna -1
+         */
+        
+        System.out.println("Es: " + obj.menosUno());
     }
 }
