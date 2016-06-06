7f4dd7d171da81bfab6130fd96b3d61162e4762b
slagogonzalez <slagogonzalez@atanasoff28.danielcastelao.org>
2016-06-06 09:29:35 +0200

    Creo el metodo sumaDiferentes y explico mediante javadoc

--- src/examenfinalparte1/Principal.java
+++ src/examenfinalparte1/Principal.java
@@ -14,7 +14,13 @@ public class Principal {
  public int suma(int num1, int num2) {
         return num1 + num2;
     }
- 
-
-  
-}
+ /**
+  * Este metodo va a sumar dos numeros distintos utilizando el metodo Suma
+  * @param num1 valor del 1 numero
+  * @param num2 valor del 2 numero
+  * @return Por ultimo retorna el valor de la suma de ambos recogida en i
+  */
+    public int SumaNumDiferentes(int num1, int num2) {
+        i = suma(num1, num2);
+        return i;
+    }
